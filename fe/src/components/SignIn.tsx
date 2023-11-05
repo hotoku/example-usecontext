4e0e0d5dbe7407933a0a69c3a73ab0bcfc47815e
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2023-11-05 15:54:04 +0900

    mock sign in

--- fe/src/components/SignIn.tsx
+++ fe/src/components/SignIn.tsx
@@ -1,7 +1,26 @@
+import { User } from "../types/user";
+import { sleep } from "../utils";
+
+async function signIn(id: string, password: string): Promise<User> {
+  console.log("sign in", id, password);
+  await sleep(1000);
+  return {
+    name: "test user",
+  };
+}
+
 function SignIn(): JSX.Element {
   return (
     <div>
-      <button>sign in</button>
+      <button
+        onClick={() => {
+          signIn("test", "test pw").then((user) => {
+            console.log(user);
+          });
+        }}
+      >
+        sign in
+      </button>
     </div>
   );
 }
